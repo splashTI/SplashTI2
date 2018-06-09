6fb53086a9e0b689a5a4e14450a2c1fa414be706
splashTI <39962337+splashTI@users.noreply.github.com>
2018-06-10 05:49:40 +1000

    Start page

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,30 @@
 var app =angular.module('SplashApp', ['ngMaterial', 'ngMessages']);
 
-app.controller('MainController',['$scope', function($scope){
+app.config(['$mdThemingProvider',function($mdThemingProvider) {
+        $mdThemingProvider.alwaysWatchTheme(true);
+        $mdThemingProvider.theme('default')
+          .primaryPalette('brown')
+          .accentPalette('deep-orange');
+}]);
+app.controller('MainController',['$scope', '$mdTheming', function($scope,$mdTheming){
 
     $scope.message = "Test";
     console.log("test test test....");
+    $mdTheming.generateTheme('default');
+   /* var removeFunction = $mdTheming.setBrowserColor({
+        theme: 'orange', // Default is 'default'
+        palette: 'red', // Default is 'primary', any basic material palette and extended palettes are available
+        hue: '200' // Default is '800'
+      });
+  
+      $scope.$on('$destroy', function () {
+        removeFunction(); // COMPLETELY removes the browser color
+      }) */
+
+      $scope.currentNavItem = 'Home';
+
+      $scope.goto = function(page) {
+        $scope.status = "Goto " + page;
+      };
 
 }]);
