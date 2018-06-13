73e270c4ee9b538ed29c37a91140c75b85e6cb49
splashTI <39962337+splashTI@users.noreply.github.com>
2018-06-14 02:50:54 +1000

    first release

--- app/js/main.js
+++ app/js/main.js
@@ -6,11 +6,72 @@ app.config(['$mdThemingProvider',function($mdThemingProvider) {
           .primaryPalette('brown')
           .accentPalette('deep-orange');
 }]);
-app.controller('MainController',['$scope', '$mdTheming', function($scope,$mdTheming){
+app.factory('appData', ['$http', function($http){
+    var obj = {content:null};
+    /*return {
+        get: function(){
+            $http.get('content.json');
+        }
 
+    }*/
+    $http.get('content.json').then(function(data){
+        console.log("test");
+        obj.content = data;
+        console.log(data);
+    });
+    return obj;
+
+}]);
+app.directive('cmdCode', function(){
+    return {
+      restrict: 'EA',
+      templateUrl: 'app/html/templates/cmd-code.html',
+      scope: {
+        cmdline: '@'
+      }
+    };
+  });
+
+app.controller('MainController',['$scope', '$mdTheming', 'appData', '$sce', function($scope,$mdTheming,appData,$sce){ 
+//app.controller('MainController',['$scope', '$mdTheming',  function($scope,$mdTheming){
     $scope.message = "Test";
     console.log("test test test....");
     $mdTheming.generateTheme('default');
+  /* appData.get().then(function(response){
+       $scope.data = response;
+   }); */
+   /*appData.then(function(response) { 
+    console.log(response);
+    $scope.data = response.data;
+   });*/
+
+   $scope.data = appData;
+ //  $scope.jsonObj = {};
+   $scope.jsonObj = appData;
+   
+   $scope.getJSON = function(){
+        if(appData){
+            //console.log('dddd');
+            console.log(appData);
+            if(appData.content){
+              //  console.log('dzzd');
+                if(appData.content.data){
+                   $scope.jsonData = appData.content.data;
+                }
+                
+            }
+            
+        }
+   };
+
+   $scope.$watch('jsonObj.content.data', function() {
+       //alert('hey, myVar has changed!');
+     $scope.getJSON();
+    // $scope.jsonData = appData.content.data;
+   });
+   $scope.trustedHtml = function (plainText) {
+            return $sce.trustAsHtml(plainText);
+        }
    /* var removeFunction = $mdTheming.setBrowserColor({
         theme: 'orange', // Default is 'default'
         palette: 'red', // Default is 'primary', any basic material palette and extended palettes are available
